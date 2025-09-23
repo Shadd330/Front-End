660216cc6b8e8e1faa9c0b95a748a2f871aec748
Anypogers <gnutzmannenzo@gmail.com>
2025-09-23 10:50:47 -0300

    portfolio page & 404 style

---
+++ src/pages/Devs/Devs.jsx
@@ -0,0 +1,16 @@
+import style from './Devs.module.css';
+
+export function Devs() {
+  return (
+    <div className={style.bg}>
+      <div className={style.pageContainer}>
+        <div className={`${style.profileBox} ${style.anypogers}`}>
+
+        </div>
+        <div className={`${style.profileBox} ${style.shadd330}`}>
+
+        </div>
+      </div>
+    </div>
+  );
+}
