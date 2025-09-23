b1becb1f87961eb5145e6c8cd11a73ae40669e12
Shadd330 <iza.gcostaramos@gmail.com>
2025-09-23 11:45:42 -0300

    Little upgrade on smaller menu

--- src/components/Smallermenu/Smallermenu.jsx
+++ src/components/Smallermenu/Smallermenu.jsx
@@ -6,10 +6,11 @@ export function Smallermenu({ active })
         
         <>
         <div className={style.Menu}>
-            <button className={style.button}><a className={` ${ style.A} ${ (active == "tasks" ? style.active : style.inactive)}`}>Active</a></button>
-            <button className={style.button1}><a className={active == "Threads" ? style.active : style.inactive}>Threads</a></button>
-            <button className={style.button2}><a className={active == "Logs" ? style.active : style.inactive}>Logs</a></button>
-            <button className={style.button3}><a className={active == "Stats" ? style.active : style.inactive}>Stats</a></button>
+            <button className={style.button}><a className={` ${ style.A} ${ (active == "tasks" ? style.active : style.inactive)}`}>Call</a></button>
+            <button className={style.button1}><a className={active == "log" ? style.active : style.inactive}>Log</a></button>
+            <button className={style.button2}><a className={active == "Steps" ? style.active : style.inactive}>Steps</a></button>
+            <button className={style.button3}><a className={active == "threads" ? style.active : style.inactive}>Threads</a></button>
+            <button className={style.button4}><a className={active == "autocalls" ? style.active : style.inactive}>Auto-Calls</a></button>
         </div>
         <div className={style.screen}></div>
         </>
