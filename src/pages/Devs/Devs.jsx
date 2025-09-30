5fb72ce9c5a7add48f8e2e267652cdb45c05c9c0
Anypogers <gnutzmannenzo@gmail.com>
2025-09-30 10:52:06 -0300

    Almost finished Devs Page

--- src/pages/Devs/Devs.jsx
+++ src/pages/Devs/Devs.jsx
@@ -6,7 +6,27 @@ export function Devs() {
       <div className={style.pageContainer}>
         <div className={[style.profileBox, style.shadd330].join(" ")}>
           <div className={style.profileContent}>
-            
+            <div className={style.userBox}>
+              <div className={style.profileImgBox}>
+                <img src="https://github.com/shadd330.png"/>
+              </div>
+              <div className={style.aboutUser}>
+                <h1>Izabella Ramos</h1>
+                <span>"..."</span>
+              </div>
+            </div>
+            <div className={style.userContribution}>
+              <h2>Responsible for the frontend</h2>
+            </div>
+            <div className={style.userBox}>
+              <div className={style.aboutUser}>
+                <h1>Shadd330</h1>
+                <span>"..."</span>
+              </div>
+              <div className={style.profileImgBox}>
+                <img src="https://github.com/shadd330.png"/>
+              </div>
+            </div>
           </div>
         </div>
         <div className={style.logoBox}>
@@ -18,7 +38,27 @@ export function Devs() {
         </div>
         <div className={[style.profileBox, style.anypogers].join(" ")}>
           <div className={style.profileContent}>
-            
+            <div className={style.userBox}>
+              <div className={style.aboutUser}>
+                <h1>Enzo Gnutzmann</h1>
+                <span>"Love with your heart and use your head for everything else"</span>
+              </div>
+              <div className={style.profileImgBox}>
+                <img src="https://github.com/anypogers.png"/>
+              </div>
+            </div>
+            <div className={style.userContribution}>
+              <h2>Responsible for the backend</h2>
+            </div>
+            <div className={style.userBox}>
+              <div className={style.profileImgBox}>
+                <img src="https://github.com/anypogers.png"/>
+              </div>
+              <div className={style.aboutUser}>
+                <h1>Anypogers</h1>
+                <span>"To be told how to live is to not live"</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
